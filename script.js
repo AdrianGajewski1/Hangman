962dd38ff1caf86a51d57385f0bcb41354d4f596
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2020-04-06 11:25:33 +0200

    finished

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const image = document.getElementById("img");
 const password_field = document.getElementById("field");
 const keys = document.getElementsByClassName("letter");
+const replay_button = document.getElementById("replay-btn");
+const game_Over_Panel = document.getElementById("gameover-panel");
+const game_result_label = document.getElementById("game-result");
 
-var password = "super dlugie haslo".toUpperCase();
+const MAX_FAILS = 5;
+var password = "haslo".toUpperCase();
 var hashedPassword = "";
 var failures = 0;
+var passwordLength = 0;
+var gameOver = false;
 
 Array.prototype.forEach.call(keys, function (el) {
   el.addEventListener("click", () => {
@@ -12,9 +18,12 @@ Array.prototype.forEach.call(keys, function (el) {
   });
 });
 
+replay_button.addEventListener("click", () => {
+  window.location.reload();
+});
+
 function hidePassword(password) {
   for (let index = 0; index < password.length; index++) {
-    if (password.charAt(index) == " ") hashedPassword += " ";
     hashedPassword += "-";
   }
 }
@@ -35,11 +44,22 @@ function check(element) {
     element.disabled = true;
     updateImg(failures);
   }
+
+  checkIfPasswordWasFound();
+
+  if (gameOver == true) {
+    game_Over_Panel.classList.add("gameOver");
+  }
 }
 
 function startUp() {
   hidePassword(password);
   password_field.innerHTML = hashedPassword;
+  var passwordSubstrings = password.split(" ");
+
+  passwordSubstrings.forEach((el) => {
+    passwordLength += el.length;
+  });
 }
 
 function setCharAt(str, index, chr) {
@@ -49,6 +69,24 @@ function setCharAt(str, index, chr) {
 
 function updateImg(fails) {
   image.src = "./img/gallows-" + fails + ".png";
+}
+
+function checkIfPasswordWasFound() {
+  var discoveredLettersCount = 0;
+
+  for (let index = 0; index < password.length; index++) {
+    if (hashedPassword.charAt(index) == "-") continue;
+    discoveredLettersCount += 1;
+  }
+
+  if (discoveredLettersCount == passwordLength) {
+    gameOver = true;
+    game_result_label.innerHTML = "You Won !!!!";
+  } else if (failures == MAX_FAILS) {
+    gameOver = true;
+    game_result_label.innerHTML = "You Lost :(";
+    game_result_label.style.color = "red";
+  }
 }
 
 window.onload = startUp;
